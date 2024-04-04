5d72a12a6d955b2a972f9d0334907b8242d98ca8
Abhinav <abhinavsl536@gmail.com>
2024-04-03 23:59:57 -0400

    Working through views in posts

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.http import JsonResponse
+from django.core import serializers
 
 # Create your views here.
 
@@ -10,7 +11,8 @@ def post_list_and_create(request):
 
 def load_post_data_view(request):
     qs = Post.objects.all()
-    return JsonResponse({'qs':qs})
+    data = serializers.serialize('json', qs)
+    return JsonResponse({'data': data})
 
 def hello_world_view(request):
     return JsonResponse({'text': 'hello world'})
