493ae4780270a6b9ebfdd2f71ee19367b85bb5a2
Abhinav <abhinavsl536@gmail.com>
2024-04-04 00:19:44 -0400

    made some changes on post array method

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -19,8 +19,6 @@ $.ajax({
     url: '/data/',
     success: function(response){
         console.log(response)
-        const data = JSON.parse(response.data)
-        console.log(data)
     },
     error: function(error){
         console.log(error)
