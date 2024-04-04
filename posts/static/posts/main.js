7032c8c80cb8ed29979a37264243c19520fc26b2
Abhinav <abhinavsl536@gmail.com>
2024-04-03 22:38:45 -0400

    testing with first Ajax Call

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -2,4 +2,15 @@ console.log('hello world')
 
 const helloWorldBox = document.getElementById('hello-world')
 
-helloWorldBox.textContent = 'hello world'
+helloWorldBox.innerHTML = 'hello <b>world</b>'
+
+$.ajax({
+    type: 'GET',
+    url: '/hello-world/',
+    success: function(response){
+        console.log(response)
+    },
+    error: function(error){
+        console.log('error', error)
+    }
+})
