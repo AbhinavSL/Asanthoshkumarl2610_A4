976e35efc169eeb8b578c178f6e8f88a4b7d8836
Abhinav <abhinavsl536@gmail.com>
2024-04-05 01:28:15 -0400

    Almost finishing with adding a form to the modal

--- posts/views.py
+++ posts/views.py
@@ -10,7 +10,7 @@ def post_list_and_create(request):
     form = PostForm(request.POST or None)
     # qs = Post.objects.all()
 
-    if request.is_ajax():
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         if form.is_valid():
             author = Profile.objects.get(user=request.user)
             instance = form.save(commit=False)
@@ -20,7 +20,7 @@ def post_list_and_create(request):
     context = {
         'form': form,
     }
-    
+
     return render(request, 'posts/main.html', context)
 
 def load_post_data_view(request, num_posts):
