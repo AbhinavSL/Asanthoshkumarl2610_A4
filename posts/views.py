a1e1180bca3d97a388b531f0261d5522fdb8e078
Abhinav <abhinavsl536@gmail.com>
2024-04-06 22:13:26 -0400

    Implemented Login

--- posts/views.py
+++ posts/views.py
@@ -4,9 +4,11 @@ from django.http import JsonResponse, HttpResponse
 from .forms import PostForm
 from profiles.models import Profile
 from .utils import action_permission
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
+@login_required
 def post_list_and_create(request):
     form = PostForm(request.POST or None)
     # qs = Post.objects.all()
@@ -30,6 +32,7 @@ def post_list_and_create(request):
 
     return render(request, 'posts/main.html', context)
 
+@login_required
 def post_detail(request, pk):
     obj = Post.objects.get(pk=pk)
     form = PostForm()
@@ -41,6 +44,7 @@ def post_detail(request, pk):
 
     return render(request, 'posts/detail.html', context)
 
+@login_required
 def load_post_data_view(request, num_posts):
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         visible = 3
@@ -61,7 +65,8 @@ def load_post_data_view(request, num_posts):
             }
             data.append(item)
         return JsonResponse({'data': data[lower:upper], 'size': size})
-    
+
+@login_required    
 def post_detail_data_view(request, pk):
     obj = Post.objects.get(pk=pk)
     data = {
@@ -73,6 +78,7 @@ def post_detail_data_view(request, pk):
     }
     return JsonResponse({'data': data})
 
+@login_required
 def like_unlike_post(request):
     # if request.is_ajax():
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
@@ -86,6 +92,8 @@ def like_unlike_post(request):
             obj.liked.add(request.user)
         return JsonResponse({'liked': liked, 'count': obj.like_count})
 
+@login_required
+@action_permission
 def update_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
@@ -99,13 +107,15 @@ def update_post(request, pk):
             'body': new_body,
         })
 
+@login_required
 @action_permission
 def delete_post(request, pk):
     obj = Post.objects.get(pk=pk)
     if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
         obj.delete()
         return JsonResponse({})
-    
+
+@login_required
 def image_upload_view(request):
     if request.method == 'POST':
         img = request.FILES.get('file')
